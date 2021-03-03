2975072041c66a876a7fab1f249e7e388201f8f2
romes <rodrigo.m.mesquita@gmail.com>
2021-03-03 00:41:35 +0000

    Solution 2

--- solutions.hs
+++ solutions.hs
@@ -1,1 +1,6 @@
+-- 1 --
 solution_1 = sum [x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
+
+-- 2 --
+fib x y = if x + y < 4000000 then x:(fib y (x+y)) else [y]
+solution_2 = sum [x | x <- (fib 1 2), x `mod` 2 == 0]
