ac338d91703507c5abf260f2d1aff0647c4da0f0
romes <rodrigo.m.mesquita@gmail.com>
2021-03-03 20:54:50 +0000

    Optimize 7

--- solutions.hs
+++ solutions.hs
@@ -1,3 +1,13 @@
+import Data.Char
+-- common --
+-- more efficient to check against a prime list than against all numbers
+-- using null xs instead of length xs == 0 (constant vs linear time) (reduced solution 7 by 6s)
+-- only checking against first half of the numbers (up to sqrt x) (a prime conjecture) is a big optimization (reduced solution 7 time by 17s)
+primeWithListOfPrimes ls x = let cmp = ceiling (sqrt (fromIntegral x)) in null [y | y <- ls, y <= cmp, mod x y == 0]
+listOfPrimes ls x = if primeWithListOfPrimes ls x 
+                       then x:(listOfPrimes (x:ls) (x+1))
+                       else listOfPrimes ls (x+1)
+
 -- 1 --
 solution_1 = sum [x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
 
@@ -28,17 +38,12 @@ solution_5 = incUntilTrue 1 (divisibleByList [1..20])
 solution_6 = abs (sum [x*x | x <- [1..100]] - ((\ x -> x * x ) (sum [1..100])))
 
 -- 7 --
--- takes 30s when compiled
-primeWithListOfPrimes ls x = length [y | y <- ls, mod x y == 0] == 0 -- more efficient than checking against all numbers
-listOfPrimes ls x = if primeWithListOfPrimes ls x 
-                       then x:(listOfPrimes (x:ls) (x+1))
-                       else listOfPrimes ls (x+1)
-solution_7 = last (take 10000 (listOfPrimes [] 2)) -- 10 000 bc 1 is not in the list but is the first prime
+-- takes 6.7 seconds when compiled 
+solution_7 = last (take 10001 (listOfPrimes [] 2))
 
 -- 8 --
 -- convert number to int list
 number8 = 7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450
-import Data.Char
 numberIntList = map (\x -> digitToInt x) (show number8)
 -- calculate a set of products of 13 digits
 multiply13digits ls = product (take 13 ls)
@@ -49,3 +54,8 @@ solution_8 = maximum (thirteenDigitProducts numberIntList [])
 
 -- 9 --
 solution_9 = head [a*b*c | c <- [1..], b <- [1..c], a <- [1..b], a*a + b*b == c*c && a+b+c == 1000]
+
+-- 10 --
+solution_10 = (sum (takeWhile (\x -> x < 2000000) (listOfPrimes [] 2))) + 1 -- too slow
+
+main = print solution_10
