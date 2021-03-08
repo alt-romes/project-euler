c2cdfae02a08739bc63125ae44c784f7cb53f6ef
romes <rodrigo.m.mesquita@gmail.com>
2021-03-08 16:32:29 +0000

    Solution 10

---
+++ solutions.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+static int* get_primes_smaller_than(int n) {
+
+    int* primes = malloc(n * sizeof(int));
+    primes[0] = 0;
+    primes[1] = 0;
+    for (int i=2; i<n; i++)
+        primes[i] = i;
+
+    int p = 2;
+    while (p*p <= n) { // p <= sqrt(n)
+
+        // If not zero then it's a prime, and we remove all its multiples bc they aren't primes
+        if (primes[p] != 0)
+            for (int i=p+p; i<n; i+=p)
+                primes[i] = 0;
+
+        p++;
+
+    }
+
+    return primes;
+    
+}
+
+static void solution_10() {
+
+    int* primes = get_primes_smaller_than(2000000);
+
+    long sum = 0;
+
+    for (int i=0; i<2000000; i++)
+        sum += primes[i];
+
+    free(primes);
+
+    printf("%ld\n", sum);
+
+}
+
+int main(int argc, char *argv[])
+{
+    solution_10(); 
+    return 0;
+}
